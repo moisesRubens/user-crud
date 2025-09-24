44f63de36023160ce3814dd845044a86656d05f2
moisesRubens <moisesrubens.pro@gmail.com>
2025-09-24 13:34:52 -0300

    implementando view index.blade

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,36 +1,67 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Todos os meus Alunos</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
+
+    <style>
+        .fixed-bottom-center {
+            position: fixed;
+            bottom: 20px;
+            left: 60px;
+            transform: translateX(-50%);
+        }
+
+        .fixed-bottom-center a.btn {
+            height: 40px;
+            /* altura do botão */
+            line-height: 40px;
+            /* centraliza texto verticalmente */
+            padding: 0 20px;
+        }
+    </style>
 </head>
+
 <body>
-    <h1>Todos os Alunos</h1>
+    <h1 class="text-center text-primary mb-4 mt-4  border-subtle">Todos os Alunos</h1>
 
-    {{-- <a href="{{ route('aluno.create') }}">Cadastrar</a> --}}
 
-    <table border="1">
-        <tr>
-            <th>ID</th>
-            <th>Nome</th>
-            <th>Idade</th>
-            <th>Ações</th>
-        </tr>
 
-        @foreach($alunos_c as $aluno)
+    <table class="table table-bordered">
+
+        <thead>
             <tr>
-                <td>{{ $aluno->id }}</td>
-                <td>{{ $aluno->name }}</td>
-                <td>{{ $aluno->age }}</td>
-                <td>
-                    {{-- <a href="{{ route('aluno.edit', $aluno->id) }}">Editar</a> 
-                    <form action="{{ route('aluno.destroy', $aluno->id) }}" method="POST" style="display:inline;">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit">Deletar</button>
-                    </form> --}}
-                </td>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>Idade</th>
+                <th>Ações</th>
             </tr>
-        @endforeach
+        </thead>
+
+        <tbody>
+            @foreach($alunos_c as $aluno)
+                <tr>
+                    <td>{{ $aluno->id }}</td>
+                    <td>{{ $aluno->name }}</td>
+                    <td>{{ $aluno->age }}</td>
+                    <td>
+                        {{-- <a href="{{ route('aluno.edit', $aluno->id) }}">Editar</a>
+                        <form action="{{ route('aluno.destroy', $aluno->id) }}" method="POST" style="display:inline;">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit">Deletar</button>
+                        </form> --}}
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
     </table>
+
+    <div class="fixed-bottom-center">
+        <a href="" class="btn btn-primary">Voltar</a>
+    </div>
+
 </body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -4,23 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AlunoController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/users', [UserController::class, 'index']);       // lista usuários
-Route::get('/users/create', [UserController::class, 'create']); // mostra form
-Route::post('/users', [UserController::class, 'store']);      // cria usuário
-Route::get('/users/{id}/edit', [UserController::class, 'edit']); // mostra form de edição
-Route::put('/users/{id}', [UserController::class, 'update']);   // atualiza usuário
-Route::delete('/users/{id}', [UserController::class, 'destroy']); // deleta usuário
-
-
-
 Route::put('/aluno/{id}', [AlunoController::class, 'update']); //backend atualiza
 Route::get('/aluno/{id}/edit', [AlunoController::class, 'edit']); //display do update
 
-Route::get('/aluno', [AlunoController::class, 'index']); //display de mostrar todos
+Route::get('/', [AlunoController::class, 'index']); //display de mostrar todos
 
 Route::post('/aluno', [AlunoController::class, 'store']); // backend de cadastro
 Route::get('/aluno/create', [AlunoController::class, 'create']);
